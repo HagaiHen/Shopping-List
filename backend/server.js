ee491e5431e5f7ef3c931a9ecf5ba757dc0514a7
hagai_hen <hagai349@gmail.com>
2024-07-30 14:37:41 +0300

    production modification

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,12 @@ import express from "express";
 import connectToMongoDB from "./db/connect.js";
 import ordersRoutes from "./routes/orders.js";
 import categoriesRoutes from "./routes/categories.js";
+import path from "path";
 
 dotenv.config(); // Loading environment variables from a .env file
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
 
 const app = express();
 
@@ -13,6 +16,12 @@ app.use(express.json()); // Middleware to parse incoming requests with JSON payl
 
 app.use("/api/orders", ordersRoutes);
 app.use("/api/categories", categoriesRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend", "dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 // Starting the server and connecting to MongoDB
 app.listen(PORT, () => {
